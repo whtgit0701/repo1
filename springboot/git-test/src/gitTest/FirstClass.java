65ed93837fb8e44a16ce202e8b4fb7ce67c54ef4
admin <admin@git.cn>
2020-03-21 21:02:35 +0800

    修改了firstClass

--- springboot/git-test/src/gitTest/FirstClass.java
+++ springboot/git-test/src/gitTest/FirstClass.java
@@ -6,4 +6,10 @@ public class FirstClass {
 
         System.out.println("hello git");
     }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
